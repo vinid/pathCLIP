f04e81b9bd4398ea115e8b4819643455b8d6a657
vinid <chiccobia@gmail.com>
2022-11-17 20:02:54 -0800

    adding augmentation

--- pathclip/model.py
+++ pathclip/model.py
@@ -4,13 +4,60 @@ from torch import optim
 import clip
 import tqdm
 import numpy as np
+from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
 import torch
+from torchvision.transforms import (
+    RandomAffine,
+    RandomPerspective,
+    RandomAutocontrast,
+    RandomEqualize,
+    RandomRotation
+)
 from pathclip.dataset import *
 from torch.utils.data import DataLoader
+from PIL import Image
+from datetime import datetime
 
-def zero_shot_classification(model, preprocess, images, labels, num_workers=1):
-    image_embeddings = image_embedder(model, preprocess,images, num_workers)
-    text_embeddings = text_embedder(model, labels, num_workers)
+from torchvision.transforms import InterpolationMode
+BICUBIC = InterpolationMode.BICUBIC
+
+
+def _convert_image_to_rgb(image):
+    return image.convert("RGB")
+
+def _train_transform(n_px):
+    return Compose([
+
+        RandomPerspective(
+            distortion_scale=0.3,
+            p=0.3,
+            interpolation=InterpolationMode.BILINEAR,
+            fill=127,
+        ),
+        RandomRotation(degrees=(0, 180)),
+        Resize(n_px, interpolation=BICUBIC),
+        CenterCrop(n_px),
+        _convert_image_to_rgb,
+        ToTensor(),
+        Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+    ])
+
+
+
+
+def _transform(n_px):
+    return Compose([
+        Resize(n_px, interpolation=BICUBIC),
+        CenterCrop(n_px),
+        _convert_image_to_rgb,
+        ToTensor(),
+        Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+    ])
+
+
+def zero_shot_classification(model, preprocess, images, labels, device, num_workers=1):
+    image_embeddings = image_embedder(model, preprocess, images, device, num_workers)
+    text_embeddings = text_embedder(model, labels, device, num_workers)
 
     score = image_embeddings.dot(text_embeddings.T)
     predictions = [labels[np.argmax(i)] for i in score]
@@ -27,14 +74,14 @@ def image_embedder(model, preprocess, list_of_images, device="cuda", num_workers
 
     total = len(list_of_images) // batch_size
     pbar = tqdm.tqdm(total=total, position=0)
+    with torch.no_grad():
+        for images in dataloader:
+            images = images.to(device)
 
-    for images in dataloader:
-        images = images.to(device)
+            image_embeddings.extend(model.encode_image(images).detach().cpu().numpy())
 
-        image_embeddings.extend(model.encode_image(images).detach().cpu().numpy())
-
-        pbar.update(1)
-    pbar.close()
+            pbar.update(1)
+        pbar.close()
 
     image_embeddings = np.array(image_embeddings)
     image_embeddings = image_embeddings / np.linalg.norm(image_embeddings, axis=1, keepdims=True)
@@ -48,14 +95,14 @@ def text_embedder(model, list_of_labels, device="cuda", num_workers=1):
     total = len(list_of_labels) // batch_size
 
     pbar = tqdm.tqdm(total=total, position=0)
+    with torch.no_grad():
+        for captions in dataloader:
+            idx = clip.tokenize(captions, truncate=True).to(device)
+            text_embeddings.extend(model.encode_text(idx).detach().cpu().numpy())
 
-    for captions in dataloader:
-        idx = clip.tokenize(captions, truncate=True).to(device)
-        text_embeddings.extend(model.encode_text(idx).detach().cpu().numpy())
+            pbar.update(1)
 
-        pbar.update(1)
-
-    pbar.close()
+        pbar.close()
 
     text_embeddings = np.array(text_embeddings)
     text_embeddings = text_embeddings / np.linalg.norm(text_embeddings, axis=1, keepdims=True)
@@ -69,13 +116,14 @@ def convert_models_to_fp32(model):
 
 class CLIPTuner:
 
-    def __init__(self, lr=5e-5, weight_decay=0.2, comet_tracking=None):
+    def __init__(self, lr=5e-5, weight_decay=0.2, comet_tracking=None, px_size=224):
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
         self.model, self.preprocess = clip.load("ViT-B/32", device=self.device,
                                                 jit=False)  # Must set jit=False for training
+        self.train_preprocess = _train_transform(px_size)
         if comet_tracking:
-            self.experiment = Experiment(comet_tracking)
+            self.experiment = Experiment(comet_tracking, project_name="pathclip")
         else:
             self.experiment = Experiment()
 
@@ -99,6 +147,8 @@ class CLIPTuner:
 
     def tuner(self, train_dataframe, validation_dataframe, save_directory, batch_size=4, epochs=5,
               evaluation_steps=500, num_workers=1):
+
+        start_time = str(datetime.now())
         train_dataset = ImageCaptioningDataset(train_dataframe, self.preprocess)
         validation_dataset = ImageCaptioningDataset(validation_dataframe, self.preprocess)
         train_dataloader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers)
@@ -167,6 +217,7 @@ class CLIPTuner:
                             if total_loss < validation_loss:
                                 validation_loss = total_loss
                                 torch.save(self.model.state_dict(), f"{save_directory}/trained_bs_{batch_size}_lr_{self.hyper_params['lr']}"
-                                                               f"_wd_{self.hyper_params['weight_decay']}.pt")
+                                                               f"_wd_{self.hyper_params['weight_decay']_}"
+                                                                    f"_{start_time}_{self.experiment.get_name()}.pt")
 
                 pbar.close()
