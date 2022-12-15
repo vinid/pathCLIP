aa423c0770cd08a6dc3ff82cea48de729b073956
vinid <chiccobia@gmail.com>
2022-12-15 09:36:59 -0800

    augmentation

--- pathclip/transform.py
+++ pathclip/transform.py
@@ -33,7 +33,10 @@ def _train_transform(n_px):
             p=0.3,
             interpolation=InterpolationMode.BILINEAR,
             fill=127,
-        )
+        ),
+        _convert_image_to_rgb,
+        ToTensor(),
+        Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
     ])
 
 
